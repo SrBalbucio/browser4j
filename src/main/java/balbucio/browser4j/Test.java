b9c01eb06991458964ecf47db7ae8ccb4f145885
João Gabriel <srbalbuciogf@gmail.com>
2026-03-28 20:35:21 -0300

    Removido classe de teste aleatoria

--- src/main/java/balbucio/browser4j/Test.java
+++
@@ -1,8 +0,0 @@
-import org.cef.network.CefCookieManager;
-import org.cef.handler.CefRequestContextHandlerAdapter;
-
-public class Test {
-    public void test() {
-        CefCookieManager cm = CefCookieManager.createManager(null, false);
-    }
-}
